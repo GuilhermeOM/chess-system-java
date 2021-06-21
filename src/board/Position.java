3c5092d3bbd48d83c4ddcc4a3d674c11d1418089
Guilherme Malta <guilherme.omalta@gmail.com>
2021-06-21 16:38:04 -0300

    Position Class

--- src/board/Position.java
+++ src/board/Position.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package board;
 
-/**
- *
- * @author guilh
- */
 public class Position {
+    private int row;
+    private int column;
     
+    public Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+    
+    public void setRow(int row){
+        this.row = row;
+    }
+    public int getRow(){
+        return row;
+    }
+    
+    public void setColumn(int column){
+        this.column = column;
+    }
+    public int getColumn(){
+        return column;
+    }
+    
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
 }
